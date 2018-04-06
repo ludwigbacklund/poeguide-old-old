1faa11207c54f8c1199ef3c6aaf3f5c148a85be3
Ludwig Bäcklund <ludli839@student.liu.se>
2018-04-06 22:35:13 +0200

    Lay groundwork for future redux-persist migrations

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { injectGlobal } from 'styled-components';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, createMigrate } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import storage from 'redux-persist/lib/storage';
 import 'antd/dist/antd.css';
@@ -13,13 +13,16 @@ import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './features';
 import globalStyles from './helpers/globalStyles';
+import migrations from './helpers/migrations';
 
 // eslint-disable-next-line
 injectGlobal`${globalStyles}`;
 
 const persistConfig = {
+  version: 0,
   key: 'timeline',
   storage,
+  migrate: createMigrate(migrations),
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -28,7 +31,7 @@ const store = createStore(
   window.devToolsExtension ? window.devToolsExtension() : f => f,
 );
 const persistor = persistStore(store);
-persistor.purge();
+// persistor.purge();
 
 ReactDOM.render(
   <Provider store={store}>
