09dac93481541bb2c5303e0ebc48311c4e095320
Ludwig Bäcklund <ludli839@student.liu.se>
2018-04-10 20:23:06 +0200

    Add styled-components babel plugin via rewire

--- config-overrides.js
+++ config-overrides.js
@@ -2,20 +2,25 @@ const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
+const rewireStyledComponents = require('react-app-rewire-styled-components');
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
     config,
   );
-
   config = rewireLess(config, env);
+  config = rewireStyledComponents(config, env, {
+    displayName: true,
+  });
 
-  config.plugins.push(new BundleAnalyzerPlugin({
-    analyzerMode: 'static',
-    reportFilename: `../build-analysis-${env || 'development'}.html`,
-    openAnalyzer: false,
-  }));
+  config.plugins.push(
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: `../build-analysis-${env || 'development'}.html`,
+      openAnalyzer: false,
+    }),
+  );
 
   return config;
 };
