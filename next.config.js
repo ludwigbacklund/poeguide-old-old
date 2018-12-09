4d17cd46c1b7e2342dedc803eb62d3be48237dc7
Ludwig Bäcklund <ludwigbacklund@gmail.com>
2018-12-09 22:27:04 +0100

    Prettify next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === 'development'
     ? {}
     : !process.env.NOW
-    ? require("next/constants")
-    : require("next-server/constants");
+    ? require('next/constants')
+    : require('next-server/constants');
 
 module.exports = phase => {
   if (phase === PHASE_PRODUCTION_SERVER) {
@@ -12,9 +12,9 @@ module.exports = phase => {
     return {};
   }
 
-  const withTypescript = require("@zeit/next-typescript");
-  const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
-  const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+  const withTypescript = require('@zeit/next-typescript');
+  const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+  const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
   return withTypescript(
     withBundleAnalyzer({
@@ -24,16 +24,16 @@ module.exports = phase => {
 
         return config;
       },
-      analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-      analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
       bundleAnalyzerConfig: {
         server: {
-          analyzerMode: "static",
-          reportFilename: "../bundles/server.html"
+          analyzerMode: 'static',
+          reportFilename: '../bundles/server.html'
         },
         browser: {
-          analyzerMode: "static",
-          reportFilename: "../bundles/client.html"
+          analyzerMode: 'static',
+          reportFilename: '../bundles/client.html'
         }
       }
     })
