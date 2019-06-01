884174ca64c6815f9e2735dd91f96a48cf856027
Ludwig Bäcklund <ludwigbacklund@gmail.com>
2019-06-01 11:12:49 +0200

    Upgrade most dependency versions

--- next.config.js
+++ next.config.js
@@ -1,46 +1,36 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { parsed: localEnv } = require('dotenv').config();
-const webpack = require('webpack');
+// const webpack = require('webpack');
+const withTypescript = require('@zeit/next-typescript');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === 'development'
-    ? {}
-    : !process.env.NOW
-    ? require('next/constants')
-    : require('next-server/constants');
+const config = {
+  webpack(config, options) {
+    if (options.isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    }
+    // config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
-module.exports = phase => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    // Fixes bug with now.sh deployment https://git.io/fp2d1
-    return {};
-  }
-
-  const withTypescript = require('@zeit/next-typescript');
-  const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
-  const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
-  return withTypescript(
-    withBundleAnalyzer({
-      webpack(config, options) {
-        if (options.isServer) {
-          config.plugins.push(new ForkTsCheckerWebpackPlugin());
-        }
-        config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-
-        return config;
-      },
-      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-      analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-      bundleAnalyzerConfig: {
-        server: {
-          analyzerMode: 'static',
-          reportFilename: '../bundles/server.html',
-        },
-        browser: {
-          analyzerMode: 'static',
-          reportFilename: '../bundles/client.html',
-        },
-      },
-    })
-  );
+    return config;
+  },
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html',
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html',
+    },
+  },
+  target: 'serverless',
+  env: {
+    GRAPHQL_API_URL: 'xxx',
+    ENGINE_API_KEY: 'xxx',
+  },
 };
+
+module.exports = withTypescript(withBundleAnalyzer(config));
