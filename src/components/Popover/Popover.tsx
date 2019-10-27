d9442c6b603c9ad85864fd07b7db9d91f90dbde0
Ludwig Bäcklund <ludwigbacklund@gmail.com>
2019-10-27 15:03:00 +0100

    Callback function and use react events instead of manual ones

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import styled from 'styled-components';
 import throttle from 'lodash/throttle';
 
@@ -21,47 +21,46 @@ export const Popover: React.SFC<PopoverProps> = ({
   const anchorRef = useRef<HTMLDivElement>(null);
   const popoverRef = useRef<HTMLDivElement>(null);
 
-  const calculateNewPopoverPosition = (
-    mouseX: number,
-    mouseY: number,
-    padding: number,
-  ) => {
-    // Re-use previously known size of the popover to prevent it from overflowing every time it re-appears
-    const popoverElementSize = {
-      width: prevPopoverElementSize.width,
-      height: prevPopoverElementSize.height,
-    };
+  const calculateNewPopoverPosition = useCallback(
+    (mouseX: number, mouseY: number, padding: number) => {
+      // Re-use previously known size of the popover to prevent it from overflowing every time it re-appears
+      const popoverElementSize = {
+        width: prevPopoverElementSize.width,
+        height: prevPopoverElementSize.height,
+      };
 
-    // Save current popover size
-    const popoverElement = popoverRef.current;
-    if (popoverElement) {
-      popoverElementSize.width = popoverElement.offsetWidth;
-      popoverElementSize.height = popoverElement.offsetHeight;
-      setPrevPopoverElementSize(popoverElementSize);
-    }
+      // Save current popover size
+      const popoverElement = popoverRef.current;
+      if (popoverElement) {
+        popoverElementSize.width = popoverElement.offsetWidth;
+        popoverElementSize.height = popoverElement.offsetHeight;
+        setPrevPopoverElementSize(popoverElementSize);
+      }
 
-    const clientSize = {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-    };
+      const clientSize = {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+      };
 
-    // Check if the current mouse position and the popover size would cause overflowing in X or Y dimensions and adjust calculated coordinates accordingly
-    let newX =
-      mouseX + popoverElementSize.width + padding > clientSize.width
-        ? clientSize.width - popoverElementSize.width
-        : mouseX + padding;
-    const newY =
-      mouseY + popoverElementSize.height + padding > clientSize.height
-        ? clientSize.height - popoverElementSize.height
-        : mouseY + padding;
+      // Check if the current mouse position and the popover size would cause overflowing in X or Y dimensions and adjust calculated coordinates accordingly
+      let newX =
+        mouseX + popoverElementSize.width + padding > clientSize.width
+          ? clientSize.width - popoverElementSize.width
+          : mouseX + padding;
+      const newY =
+        mouseY + popoverElementSize.height + padding > clientSize.height
+          ? clientSize.height - popoverElementSize.height
+          : mouseY + padding;
 
-    // Check if the mouse would overlap with the popover and flip the popover to the other side of the cursor if so
-    if (mouseX >= newX) newX = mouseX - popoverElementSize.width - padding;
+      // Check if the mouse would overlap with the popover and flip the popover to the other side of the cursor if so
+      if (mouseX >= newX) newX = mouseX - popoverElementSize.width - padding;
 
-    return { newX, newY };
-  };
+      return { newX, newY };
+    },
+    [],
+  );
 
-  const onMouseMove = throttle(e => {
+  const onMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const padding = 20;
 
     const mouseX = e.clientX;
@@ -76,23 +75,7 @@ export const Popover: React.SFC<PopoverProps> = ({
       zIndex: 999,
     });
     setShouldRenderPopover(true);
-  }, 15);
-
-  useEffect(() => {
-    const anchorElement = anchorRef.current;
-
-    if (anchorElement) {
-      anchorElement.addEventListener('touchstart', e => {
-        e.preventDefault();
-      });
-      anchorElement.addEventListener('mousemove', onMouseMove);
-      anchorElement.addEventListener('mouseout', () => {
-        setShouldRenderPopover(false);
-      });
-
-      return () => anchorElement.removeEventListener('mousemove', onMouseMove);
-    }
-  }, []);
+  };
 
   return (
     <>
@@ -101,7 +84,13 @@ export const Popover: React.SFC<PopoverProps> = ({
           {content}
         </PopoverWrapper>
       )}
-      <Anchor className={className} ref={anchorRef}>
+      <Anchor
+        onMouseMove={throttle(onMouseMove, 15)}
+        onMouseOut={() => setShouldRenderPopover(false)}
+        onTouchStart={e => e.preventDefault()}
+        className={className}
+        ref={anchorRef}
+      >
         {children}
       </Anchor>
     </>
@@ -110,6 +99,7 @@ export const Popover: React.SFC<PopoverProps> = ({
 
 const PopoverWrapper = styled.div`
   z-index: 10;
+  pointer-events: none;
 `;
 
 const Anchor = styled.div`
