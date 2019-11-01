a42dfe1c13aeedc451c8ce7b2e83e74c6555e6e5
Ludwig Bäcklund <ludwigbacklund@gmail.com>
2019-11-02 00:07:10 +0100

    Add scaling on hover to Gem

--- src/components/Gems/Gem/Gem.tsx
+++ src/components/Gems/Gem/Gem.tsx
@@ -20,6 +20,12 @@ export const Gem = memo(GemComponent);
 const GemWrapper = styled.div`
   display: flex;
   align-items: center;
+  transition: all 0.3s ease-in-out;
+
+  :hover {
+    transform: scale(1.1, 1.1);
+    cursor: pointer;
+  }
 `;
 
 const GemName = styled.span`
